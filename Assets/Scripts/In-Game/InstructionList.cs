62cb76e0914d5ba3093ac6d3f980d604810773e8
juliorkm <julioramakrsnamandoju@gmail.com>
2020-04-20 09:09:42 -0300

    Fixed issue when trying to place a sixth instruction

--- Assets/Scripts/In-Game/InstructionList.cs
+++ Assets/Scripts/In-Game/InstructionList.cs
@@ -7,6 +7,7 @@ using UnityEngine.EventSystems;
 public class InstructionList : MonoBehaviour {
 
     public List<GameObject> list;
+    public int maxInstructions = 5;
 
     [SerializeField]
     private GameObject instPrefab;
@@ -16,47 +17,51 @@ public class InstructionList : MonoBehaviour {
 
     public void insertAtPosition(int position) {
         list.Remove(null);
-        if (position > list.Count) position = list.Count;
-        if (toBeAdded != null) {
-            var father = GameObject.Find("Node" + position).transform;
-            if (father.childCount > 0) Destroy(father.GetChild(0));
-            var go = Instantiate(instPrefab,
-                             Utils.findChild(gameObject, "Node" + position).transform
-                             );
-            var node = go.GetComponent<InstructionListNode>();
-            node.instructionList = this;
-            node.index = position;
-            node.instructionType = toBeAdded.GetComponent<DraggedInstruction>().type;
+        if (list.Count < maxInstructions) {
+            if (position > list.Count) position = list.Count;
+            if (toBeAdded != null) {
+                var father = GameObject.Find("Node" + position).transform;
+                if (father.childCount > 0) Destroy(father.GetChild(0));
+                var go = Instantiate(instPrefab,
+                                 Utils.findChild(gameObject, "Node" + position).transform
+                                 );
+                var node = go.GetComponent<InstructionListNode>();
+                node.instructionList = this;
+                node.index = position;
+                node.instructionType = toBeAdded.GetComponent<DraggedInstruction>().type;
 
-            var image = go.GetComponent<Image>();
-            var draggedImage = toBeAdded.GetComponent<Image>();
-            var color = draggedImage.color;
-            color.a = 1;
-            image.color = color;
-            image.sprite = draggedImage.sprite;
+                var image = go.GetComponent<Image>();
+                var draggedImage = toBeAdded.GetComponent<Image>();
+                var color = draggedImage.color;
+                color.a = 1;
+                image.color = color;
+                image.sprite = draggedImage.sprite;
 
-            var icon = go.transform.GetChild(0).GetComponent<Image>();
-            var draggedIcon = toBeAdded.transform.GetChild(0).GetComponent<Image>();
-            icon.sprite = draggedIcon.sprite;
-            icon.transform.rotation = draggedIcon.transform.rotation;
-            node.dragItemIcon = icon.sprite;
+                var icon = go.transform.GetChild(0).GetComponent<Image>();
+                var draggedIcon = toBeAdded.transform.GetChild(0).GetComponent<Image>();
+                icon.sprite = draggedIcon.sprite;
+                icon.transform.rotation = draggedIcon.transform.rotation;
+                node.dragItemIcon = icon.sprite;
 
-            node.rotation = draggedIcon.transform.eulerAngles.z;
+                node.rotation = draggedIcon.transform.eulerAngles.z;
 
-            go.transform.localPosition = Vector2.zero;
+                go.transform.localPosition = Vector2.zero;
 
-            list.Insert(position, go);
+                list.Insert(position, go);
+            }
         }
     }
 
     public void hoverPositionEnter(int position) {
-        if (DragDropManager.currentDragItem != null) {
-            list.Remove(null);
-            if (position > list.Count) position = list.Count;
-            list.Insert(position,
-                        null
-                        );
-            toBeAdded = DragDropManager.currentDragItem;
+        if (list.Count < maxInstructions) {
+            if (DragDropManager.currentDragItem != null) {
+                list.Remove(null);
+                if (position > list.Count) position = list.Count;
+                list.Insert(position,
+                            null
+                            );
+                toBeAdded = DragDropManager.currentDragItem;
+            }
         }
     }
 
