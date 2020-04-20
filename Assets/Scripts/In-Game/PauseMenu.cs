a0e960d2808b8b597c473fb34f7fd9201fd73a39
juliorkm <julioramakrsnamandoju@gmail.com>
2020-04-20 06:22:45 -0300

    Created puzzle selection menu and pause menu

---
+++ Assets/Scripts/In-Game/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+    [SerializeField]
+    private GameObject menu;
+
+    public void openPauseMenu() {
+        Time.timeScale = 0;
+        menu.SetActive(true);
+    }
+
+    public void closePauseMenu() {
+        Time.timeScale = 1;
+        menu.SetActive(false);
+    }
+
+    public void toMainMenu() {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Scenes/PuzzleMenu");
+    }
+}
---
+++ Assets/Scripts/PuzzleLoader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleLoader : MonoBehaviour {
+    public void loadPuzzle(string a) {
+        Utils.loadPuzzle("PuzzleStage" + a);
+    }
+}
